9055ecf0617cab4c3ef9883d0b79d5e051e589ef
egitelma <egitelma@ucsc.edu>
2024-11-03 12:10:57 -0800

    L16 Dialog complete; Setup + a simple alert button

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-// todo
+//L16 Dialog - add button (setup)
+
+const APP_NAME = "Geocacher";
+const app = document.querySelector<HTMLDivElement>("#app")!;
+const test_button = document.createElement("button");
+test_button.innerHTML = "Click Here!";
+
+app.append(test_button);
+
+test_button.addEventListener("click", () => {
+  alert("Welcome to " + APP_NAME + "!");
+});
